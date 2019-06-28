dd84970d7c52dd6b3a553811f1594b3857fe7b1b
Lindsay Preston <lindsayarthur303@gmail.com>
2019-06-28 16:58:45 -0600

    gif file

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burger;
+USE burger;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
