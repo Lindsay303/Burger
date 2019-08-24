5041b69c50eab73f12670f0658c5fd6a7a730320
Lindsay Preston <lindsayarthur303@gmail.com>
2019-08-24 12:59:39 -0600

    Please work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,17 @@
+// All of our router gets, posts, and puts are in this file
+
+// Controllers act as an interface between Model and View components to process all the business logic and incoming requests, manipulate data using the Model component and interact with the Views to render the final output. For example, the Customer controller will handle all the interactions and inputs from the Customer View and update the database using the Customer Model. The same controller will be used to view the Customer data.
+
 var express = require("express");
 var router = express.Router();
-var burger = require("../models/burgers.js");
 
-// Create routes.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+  //Pass the burger data into the function
+  burger.selectAll(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -13,67 +20,33 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
+router.post("/burgers/insertOne", function (req, res) {
+  burger.insertOne([
+    "burger_name"
   ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
-    // Return ID of the Burger
-    res.json({ id: result.insertId });
-  });
-});
-
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      console.log(result);
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-  console.log('delete');
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      console.log(result);
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.get("/api/burgers/:id", function(req, res) {
-  console.log('get api_burgers_id');
-  var condition = "id = " + req.params.id;
-  
-  console.log("condition", condition);
-  
-  burger.read(
-    condition,
-    function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.json({ id: result.insertId,
-                 burger_name: result.burger_name,
-                 devoured: result.devoured });
-//      res.status(200).end();
+      req.body.burger_name
+    ], function (data) {
+      res.redirect('/');
     });
+});
+
+router.put("/burgers/updateOne/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  burger.updateOne({
+    devoured: true
+  }, condition, function (data) {
+    res.redirect('/');
+  });
+});
+
+router.delete("/burgers/deleteAll/", function (req, res) {
+  burger.deleteAll(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.redirect('/');
+  });
 });
 
 
--- server.js
+++ server.js
@@ -1,30 +1,38 @@
+// 1) We collect data using the ORM
+// 2) Send it into burger.js
+// 3) Use burger.js to send the data to the front-end
+
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 3000;
-
+var PORT = process.env.PORT || 8080;
 var app = express();
 
-// Serve static content from "public"
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// parser
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars
-var exphbs = require("express-handlebars");
+app.use(methodOverride('_method'));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// We set this as the view engine bc Handlebars is controlling what the users see
 app.set("view engine", "handlebars");
 
-// Import routes
+// Import routes and give the server access to them. This is where all of our express routes are.
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
+  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
 
